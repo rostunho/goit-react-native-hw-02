3c8158774fa1d68f1fb764f00a305f40c0c3e649
Rostyk Podolets <ros.podolets@gmail.com>
2023-03-18 00:40:35 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  Platform,
+  KeyboardAvoidingView,
+  Dimensions,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+import RegistrationScreen from "./screens/RegistrationScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.imageBg}
+          source={require("./assets/images/photo-bg.jpg")}
+        >
+          <KeyboardAvoidingView
+            style={styles.avoidingView}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+          >
+            {/* <RegistrationScreen /> */}
+            <LoginScreen />
+          </KeyboardAvoidingView>
+
+          <StatusBar style="auto" />
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "flex-end",
+  },
+  imageBg: {
+    flex: 1,
+    resizeMode: "cover",
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+    // justifyContent: "flex-end",
+    // alignItems: "center",
+  },
+  avoidingView: {
+    // flex: 1,
+    width: "100%",
+    height: "100%",
+    justifyContent: "flex-end",
   },
 });
